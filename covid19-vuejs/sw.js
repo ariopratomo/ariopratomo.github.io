6684235c7b408be1b1457e449e963dbb4c670039
Ario Pratomo <mrx.anupak@gmail.com>
2020-03-30 02:43:08 +0700

    g

--- covid19-vuejs/sw.js
+++ covid19-vuejs/sw.js
@@ -29,7 +29,7 @@ self.addEventListener('fetch', function (event) {
       return fetch(event.request);
     }).catch(function () {
       // If both fail, show a generic fallback:
-      return caches.match('offline.html');
+      return caches.match('./offline.html');
       // However, in reality you'd have many different
       // fallbacks, depending on URL & headers.
       // Eg, a fallback silhouette image for avatars.
