72365c0d904c1fe28cfa18df4e38fdeb66e1339f
Ario Pratomo <mrx.anupak@gmail.com>
2020-03-30 01:51:27 +0700

    commit pwa

---
+++ covid19-vuejs/service-worker.js
@@ -0,0 +1,47 @@
+// const FILES_TO_CACHE = [
+//   'offline.html',
+// ];
+// evt.waitUntil(
+//   caches.open(CACHE_NAME).then((cache) => {
+//     console.log('[ServiceWorker] Pre-caching offline page');
+//     return cache.addAll(FILES_TO_CACHE);
+//   })
+// );
+var CACHE_NAME = 'covid-19-vue-cache';
+var urlsToCache = [
+  'offline.html',
+  'assets/css/offline.css'
+];
+
+self.addEventListener('install', function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  console.log('Finally active. Ready to start serving content!');
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    }).catch(function () {
+      // If both fail, show a generic fallback:
+      return caches.match('offline.html');
+      // However, in reality you'd have many different
+      // fallbacks, depending on URL & headers.
+      // Eg, a fallback silhouette image for avatars.
+    })
+  );
+});
